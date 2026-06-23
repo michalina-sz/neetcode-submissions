3cda13ae5344a6b84e48b4ce3f7ed041e84de85c
Michalina Szostak <42873374+michalina-sz@users.noreply.github.com>
2026-06-23 18:14:33 +0200

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let count = 0;
+        let max = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === 1) {
+                count++;
+            } else {
+                max = Math.max(count, max)
+                count = 0;
+            }
+        }
+        return max > count ? max : count
+    }
+}
